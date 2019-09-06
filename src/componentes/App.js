782df514577b86ae83d37551340aeb873e838278
joseclaros <51924203+joseclaros@users.noreply.github.com>
2019-09-06 18:20:03 -0400

    nuevo

---
+++ src/componentes/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+
+
+
+
+
+class App extends Component{
+    
+    
+    
+    state = {
+        task : {
+            title: 'Mi primera tarea',
+            done: true
+        }
+    }
+    
+    //Variable boton
+    handleClick = () => {
+        const task = this.state.task;
+        task.done = !task.done;
+        this.setState({task});
+    }
+
+    
+    render(){
+        return(
+            <div>
+                <h3>{this.state.task.title}</h3>
+                <h1>{this.state.task.done.toString()}</h1>
+                <button onClick ={this.handleClick}>
+                    click
+                </button>
+            </div>
+            
+        )
+    }
+
+
+
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*import React from 'react';
+import { render } from 'react-dom';
+
+import App from './componentes/App';
+
+render(
+    <div>
+        <h1>Mi Pagina Web</h1>
+        <App/>
+    </div>, 
+    document.getElementById('app')
+);*/
+
+
+
+//console.log('Aplicación corriendo');
